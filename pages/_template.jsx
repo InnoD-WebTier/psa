5829292bc4575f4f150c6b086666b284b3df04dd
Benjamin Marimon <bsmarimon@berkeley.edu>
2017-02-27 13:32:14 -0800

    Landing page prototype finished

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -5,22 +5,89 @@ import Headroom from 'react-headroom'
 import '../css/markdown-styles';
 import '../css/main';
 
-module.exports = React.createClass({
-  propTypes () {
-    return {
-      children: React.PropTypes.any,
-    }
-  },
-  render () {
+// Navigation bar adapted from navigation written by Dennis Moon
+// He's a really great frontend developer, check him out!
+// https://github.com/bohuim
+
+// import page names
+import data from './data/nav.js';
+
+class NavLink extends React.Component {
+  render() {
+    let name = this.props.name;
+    let link = "/";
+    if (name && name.length > 0)
+    link += name + "/";
+
+    let active = this.props.active ? ' active' : '';
+
+    return (
+      <Link
+        to = {prefixLink(link)}
+        className = {'nav-link' + active}
+        onClick={this.props.clickHandler}
+        >
+        {name}
+      </Link>
+    );
+  }
+}
+
+class Template extends React.Component {
+  constructor(props) {
+    super(props);
+    // get pathname and strip all '/'
+    let page = this.props.location.pathname.replace(new RegExp('/', 'g'), '');
+    this.state = {
+        page: page,
+        showMenu: false,
+    };
+  }
+
+  getNavLinks() {
+    let page = this.props.location.pathname.replace(new RegExp('/', 'g'), '');
+
+    return data['links'].map(link => (
+      <NavLink
+        name = {link}
+        key  = {'nav-' + link}
+        active = {link === page}
+        clickHandler = {() => this.toggleMenu()}
+      />
+    ));
+  }
+
+  toggleMenu()
+  {
+    this.setState({showMenu: !this.state.showMenu});
+  }
+
+  render() {
+    let showMenu = this.state.showMenu ? ' show' : ' hide';
     return (
       <div>
-        <div className="header-container">
-          <p className="header">Berkeley Bug Map</p>
-        </div>
-        <div>
+        <Headroom>
+          <div className="navbar">
+            <div className="nav-content">
+              <Link className="badge" to={prefixLink("/")}>
+                <img className="logo" src={prefixLink("/assets/logo.png")} />
+              </Link>
+              <nav className={"nav-links " + showMenu}>
+                {this.getNavLinks()}
+              </nav>
+              <img src={prefixLink("/assets/menu.png")}
+                className="menu-button"
+                onClick={() => this.toggleMenu()}
+              />
+            </div>
+          </div>
+        </Headroom>
+        <div className={this.props.location.pathname === prefixLink("/") ? "" : "content"}>
           {this.props.children}
         </div>
       </div>
-    )
-  },
-})
+    );
+  }
+}
+
+export default Template;
--- pages/index.js
+++ pages/index.js
@@ -6,40 +6,18 @@ import { config } from 'config'
 
 export default class Index extends React.Component {
 
-  componentDidMount() {
-    const uluru = {lat: 37.8713466, lng: -122.2589184};
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: uluru
-    });
-    const markers = [
-      {lat: 37.8713466, lng: -122.2589184, string: "<p>Big memes</p>"},
-      {lat: 37.872, lng: -122.251, string: "<p>Small memes</p>"},
-    ];
-    for (let position of markers) {
-      const infowindow = new google.maps.InfoWindow({
-        content: position.string,
-      });
-      const marker = new google.maps.Marker({
-        position,
-        map: map
-      });
-      marker.addListener('click', function() {
-        infowindow.open(map, marker);
-      });
-    }
-  }
-
   render () {
     return (
-      <div>
-        <Helmet
-          title={config.siteTitle}
-        />
-        <div className="map-container">
-          <div id="map"></div>
+      <div className="landing-container">
+        <p className="title">Progressive Student Association</p>
+        <div className="signup-container">
+          <input
+            placeholder="Leave us your email and we'll get in touch!"
+          />
+          <div className="submit-btn">
+            <span>Submit</span>
+          </div>
         </div>
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAK-0aO8lrEis8_DuFJj8EevofQjhOh9Oc"></script>
       </div>
     )
   }
