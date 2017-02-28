fbee038b982797ccc3e7af627435e9244db4e520
Benjamin Marimon <bsmarimon@berkeley.edu>
2017-02-28 15:00:18 -0800

    About page logic done and rresponsive

--- pages/about.js
+++ pages/about.js
@@ -4,12 +4,71 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import { config } from 'config'
 
+class Officer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div className="officer">
+        <div className="potrait-container">
+          <img className="portrait" src={prefixLink("/assets/logo.png")} /> <br/>
+        </div>
+        <span className="name">{this.props.name}</span>
+        <span className="position">{this.props.position}</span>
+      </div>
+    );
+  }
+}
+
+
+
 export default class About extends React.Component {
   render () {
     return (
-      <div className="about content">
-        <p className="title">Hi</p>
-        <p className="desc">The Progressive Student Association (PSA) was originally founded as UC Berkeley for Bernie (UCB4B). After the primary election, we reorganized ourselves as PSA in order to continue the political revolution at UC Berkeley. For info on how to get involved, comments, concerns, and questions, please do not hesitate to message this page, or email us at <span className="highlight">ucberkeleypsa@gmail.com</span>.</p>
+      <div>
+        <div className="about content">
+          <p className="title">Here&#39;s a quick introduction.</p>
+          <p className="desc">The Progressive Student Association (PSA) was originally founded as UC Berkeley for Bernie (UCB4B). After the primary election, we reorganized ourselves as PSA in order to continue the political revolution at UC Berkeley. For info on how to get involved, comments, concerns, and questions, please do not hesitate to message this page, or email us at <span className="highlight">ucberkeleypsa@gmail.com</span>.</p>
+        </div>
+        <div className="officers-container">
+          <p className="title">Team</p>
+          <div className="officer-list">
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+            <Officer
+              name={"Natasha Welingkar"}
+              position={"President"}
+            />
+          </div>
+        </div>
       </div>
     )
   }
